094c0c107fb6da08aa225ea252f46ca2bfcd013a
Arnoldo Cantu <arnoldo.cantu17@yahoo.com>
2022-10-02 18:37:30 -0500

    Completed router changes

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>404, Page not found</h1>
+  <h1 class="not-found">404: Page not found</h1>
 </template>
+
+<style lang="postcss" scoped>
+  .not-found {
+    @apply pt-80 text-center text-3xl font-bold;
+  }
+</style>
